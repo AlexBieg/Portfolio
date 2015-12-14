9df066b80fd246b272863a8ee202ad6f65d74c8b
AlexBieg <alexbieg95@gmail.com>
2015-12-14 15:28:26 -0800

    asdf

--- portfolio/baby-names/names.js
+++ portfolio/baby-names/names.js
@@ -19,7 +19,7 @@ Javascript for babaynames website
 		if(name){
 			var ajax = new XMLHttpRequest();
 			ajax.onload = addData;
-			ajax.open("GET", "getnames.php?name=" + name + "&gender=" + gender);
+			ajax.open("GET", "http://www.alexbieg.com/portfolio/getnames.php?name=" + name + "&gender=" + gender);
 			ajax.send();
 		}
 		return false;
